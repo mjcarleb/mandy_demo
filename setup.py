d4252cd265f72a6ae927c927eb2cc95b0419e53a
Mark Carlebach <markjcarlebach@gmail.com>
2020-06-22 07:53:18 -0400

    playing with setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='Demo of project lifecycle',
-    author='Mark Carlebach',
-    license='MIT',
+   name='my_numpy',
+   version='1.0',
+   description='Learning to create packages mimicing numpy',
+   author='Mark Carlebach',
+   author_email='markjcarelbach@gmail.com',
+   packages=['my_numpy'],  #same as name
+   install_requires=['numpy', 'pandas'], #external packages as dependencies
 )
