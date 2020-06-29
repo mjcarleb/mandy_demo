28e8ea127ffb08c340592eaa3b2ba0e6a56739e6
Mark Carlebach <markjcarlebach@gmail.com>
2020-06-29 06:36:25 -0400

    Moved package out

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
+from my_numpy.Array import my_ndarray as mnp
+a = mnp(l=[1, 2, 3, 4, 5])
+print(f"The shape of our mnp = {a.shape()}")
+print()
+
 import os, sys
 
 # Remove /home/mjcarleb/mandy_demo from sys.path
 path = sys.path
-print(path[1])
-nl = [sys.path[0]]
-nl.append(sys.path[2:])
-sys.path = nl
+print("Sys Path:")
 print(sys.path)
-
-from my_numpy.Array import my_ndarray as mnp
-a = mnp(l=[1, 2, 3, 4, 5])
-print(f"The shape of our mnp = {a.shape()}")
-print(f"current directory = {os.getcwd()}")
-print(f"current path = {sys.path}")
+print()
+print("Current Directory:")
+print(os.getcwd())
