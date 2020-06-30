2cfdaedb13e7941b95608ce6ce2ac99a71e2f93c
Mark Carlebach <markjcarlebach@gmail.com>
2020-06-30 08:23:20 -0400

    Working with generators

--- src/generators.py
+++ src/generators.py
@@ -4,6 +4,9 @@ def squares_gen():
 
     i = 0
     while True:
+
+        # Python stops here until next is applied to generator
+        # After next is applied, execution continues until the next yield
         yield i**2
         i += 1
 
@@ -17,13 +20,21 @@ def fib_gen():
 
     """Generate an infinite sequence of fibinocci numbers"""
 
+    # Python stops here until next is applied to generator
+    # After next is applied, execution continues until the next yield
     yield 1
+
+    # Python stops here until next is applied to generator
+    # After next is applied, execution continues until the next yield
     yield 1
 
     a=1
     b=1
     while True:
         c = a + b
+
+        # Python stops here until next is applied to generator
+        # After next is applied, execution continues until the next yield
         yield c
         a = b
         b = c
