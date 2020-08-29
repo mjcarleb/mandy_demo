0134990cd6192172cc552f24cdcb8d450572378f
Mark Carlebach <markjcarlebach@gmail.com>
2020-08-29 16:10:29 -0400

    on phone with Mandy

---
+++ src/docker_demo.py
@@ -0,0 +1,6 @@
+import sys
+import dask, flask, numpy as np
+print(f"sys: {sys.version}")
+print(f"dask:  {dask.__version__}")
+print(f"flask:  {flask.__version__}")
+print(f"numpy:  {np.__version__}")
